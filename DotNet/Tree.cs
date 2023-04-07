d15cc8aaec2c6cc62c0d3cbca0ba7f4bef16f649
Sergei Kamianetskii <i@ksergey.ru>
2023-04-07 03:15:06 +0300

    Добавлен способ обхода дерева для последующей печати

--- DotNet/Tree.cs
+++ DotNet/Tree.cs
@@ -3,4 +3,25 @@ using System.Text;
 public class Tree<T>
 {
   public TreeNode<T> Root { get; set; }
+
+  public string Print()
+  {
+    StringBuilder sb = new();
+    TreeTraversal(this.Root, sb);
+    return sb.ToString();
+  }
+
+  private void TreeTraversal<T>(TreeNode<T> node,
+                                StringBuilder sb,
+                                string sp = "")
+  {
+    if (node != null)
+    {
+      sb.Append($"{sp}[{node.Value}]\n");
+      foreach (var child in node.Children)
+      {
+        TreeTraversal(child, sb, $"{sp}  ");
+      }
+    }
+  }
 }
